6a588b222225b72587b0c291d7d410e01752fa07
fandikaikhsan <fandika.ikhsan@gmail.com>
2026-01-28 16:29:02 -0500

    feat: init the streamlit

---
+++ streamlit_app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.title("Streamlit -> Langgraph dev (Threads + Runs)")
+st.write(
+    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+)
+
